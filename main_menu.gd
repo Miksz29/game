cea07b412e88264dfa1bdcdd43cbf7bfa92d06e7
Glenn <glennmichaelorlanes@gmail.com>
2026-09-20 14:17:52 +0800

    script bug fixes

--- main_menu.gd
+++ main_menu.gd
@@ -13,8 +13,6 @@ func _on_start_pressed() -> void:
 	$fade_transition.show()
 	$fade_transition/Fade_Timer.start()
 	$fade_transition/AnimationPlayer.play("fade_out")
-func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/levels/level_1.tscn")
 
 
 func _on_exit_pressed() -> void:
