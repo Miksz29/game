d1bb807fa4fae4a9c92f6c132c80e7bce05004a6
Glenn <glennmichaelorlanes@gmail.com>
2026-09-20 22:57:04 +0800

    added continue button that saves last level loaded/ modified pause menu

--- main_menu.gd
+++ main_menu.gd
@@ -5,21 +5,29 @@ var button_type = null
 
 func _ready() -> void:
 	Bgm.stop()
-	$AudioStreamPlayer.play()
+	print("HAS SAVE: ", SaveManager.has_save())
+	$Button_Manager/Continue.visible = SaveManager.has_save()
 
 
 func _on_start_pressed() -> void:
 	button_type = "start"
+	SaveManager.start_level = 1
 	$fade_transition.show()
 	$fade_transition/Fade_Timer.start()
 	$fade_transition/AnimationPlayer.play("fade_out")
-
+	
+func _on_continue_pressed() -> void:
+	button_type = "continue"
+	SaveManager.start_level = SaveManager.load_level()
+	$fade_transition.show()
+	$fade_transition/Fade_Timer.start()
+	$fade_transition/AnimationPlayer.play("fade_out")
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
 
 
 func _on_fade_timer_timeout() -> void:
-	if button_type == "start":
+	if button_type == "start" or button_type == "continue":
 		get_tree().create_timer(1.0).timeout.connect(Bgm.play)
 		get_tree().change_scene_to_file("res://scenes/main.tscn")
---
+++ scripts/save_manager.gd
@@ -0,0 +1,19 @@
+extends Node
+
+const SAVE_PATH = "user://save.cfg"
+var start_level: int = 1
+
+func save_level(level: int) -> void:
+	var cfg = ConfigFile.new()
+	cfg.set_value("progress", "level", level)
+	var err = cfg.save(SAVE_PATH)
+	print("SAVE: level ", level, " | error code ", err, " | ", ProjectSettings.globalize_path(SAVE_PATH))
+
+func has_save() -> bool:
+	return FileAccess.file_exists(SAVE_PATH)
+
+func load_level() -> int:
+	var cfg = ConfigFile.new()
+	if cfg.load(SAVE_PATH) != OK:
+		return 1
+	return cfg.get_value("progress", "level", 1)
